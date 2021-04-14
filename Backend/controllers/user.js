0d1d66957b1ef996b54ce9e543c7c7d56756b1b0
Rheeda-boo <rheeda.beecha@gmail.com>
2021-04-14 10:32:50 +0000

    signup page added

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -3,13 +3,13 @@ const bcrypt = require("bcrypt");
 
 exports.getLogin = (req, res, next) => {
     res.send("Login Page");
-}
+};
 
 exports.postLogin = (req, res, next) => {
     const email  = req.body.email;
     const password = req.body.password;
 
-    console.log(password, email);
+    // console.log(password, email);
 
     let errors = [];
 
@@ -47,3 +47,63 @@ exports.postLogin = (req, res, next) => {
     };
     
 };
+
+exports.getSignUp = (req, res, next) => {
+    res.send("SignUp Page");
+};
+
+exports.postSignUp = (req, res, next) => {
+    const name  = req.body.name;
+    const email  = req.body.email;
+    const password = req.body.password;
+    const password2 = req.body.password2;
+
+    let errors = [];
+
+    if (!name || !email || !password || !password2) {
+        res.send({ msg: "Please fill in all fields" });
+    }
+
+    else if (password !== password2) {
+        res.send({ msg: "Passwords do not match" });
+    }
+
+    else if (password.length < 6) {
+        res.send({ msg: "Password must be at least 6 characters" });
+    }
+    else {
+        User.findOne({email: email}).exec((err, user) => {
+            if (user) {
+                res.send("Already a User");
+            }
+            else {
+                const newUser = new User({
+                    name: name,
+                    email: email,
+                    password: password,
+                    role: "student",
+                });
+
+                bcrypt.genSalt(10, (error, salt) => {
+                    bcrypt.hash(newUser.password, salt, (error, hash) => {
+                        if (error) {
+                            throw error
+                        }
+                        else {
+                            newUser.password = hash;
+
+                            newUser
+                            .save()
+                            .then((value) => {
+                                res.send({msg: "SignUp successful"})
+                            })
+                            .catch((value) => console.log(value));
+                        };
+                    });    
+                });
+            };
+        });
+    };
+
+};
+
--- Backend/routers/users.js
+++ Backend/routers/users.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const userController = require("../controllers/user");
 
 router.get("/login", userController.getLogin);
+router.get("/signup", userController.getSignUp);
 router.post("/login", userController.postLogin);
+router.post("/signup", userController.postSignUp);
+
 
 
 module.exports = router;
